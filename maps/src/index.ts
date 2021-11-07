bfda92be4fee5e0479bb416b553a7d4778bdd0b3
Chiang Lynn <lynnchiangtw@gmail.com>
2021-11-07 16:39:51 +0800

    Create CustomMap

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,6 @@
-import { User } from './User';
-import { Company } from './Company';
+/// <reference types="@types/google.maps" />
+// import { User } from './User';
+// import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+new CustomMap('map');
