f6b7dcc9c5e400fabebec91c75007baf5c09c857
Chiang Lynn <lynnchiangtw@gmail.com>
2021-11-07 22:40:42 +0800

    Add markers on customMap

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,3 +1,13 @@
+import { User } from './User';
+import { Company } from './Company';
+
+interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    };
+}
+
 export class CustomMap {
     private googleMap: google.maps.Map;
 
@@ -8,6 +18,16 @@ export class CustomMap {
                 lat: 0,
                 lng: 0,
             },
+        });
+    }
+
+    addMarker(mappable: Mappable): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng,
+            }
         });
     }
 }
